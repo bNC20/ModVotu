1c8e2057884174f9c38878d74f8ef243abd635cc
bNC20 <bruno@AsusVivobook-Bruno>
2021-08-04 16:35:45 -0300

    Blockzap

--- src/main/java/net/mcreator/votucraft/block/ZapblockBlock.java
+++ src/main/java/net/mcreator/votucraft/block/ZapblockBlock.java
@@ -33,7 +33,7 @@ public class ZapblockBlock extends VotucraftModElements.ModElement {
 	}
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
 			setRegistryName("zapblock");
 		}
 
