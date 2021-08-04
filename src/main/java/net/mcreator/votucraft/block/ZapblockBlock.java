7e0deef97276e98a63ac438cfd96eca12e044c40
nickoioooo <Usuario@192.168.0.104>
2021-08-04 16:34:47 -0300

    bloco de zap

---
+++ src/main/java/net/mcreator/votucraft/block/ZapblockBlock.java
@@ -0,0 +1,48 @@
+
+package net.mcreator.votucraft.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.votucraft.itemgroup.VotucraftItemGroup;
+import net.mcreator.votucraft.VotucraftModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@VotucraftModElements.ModElement.Tag
+public class ZapblockBlock extends VotucraftModElements.ModElement {
+	@ObjectHolder("votucraft:zapblock")
+	public static final Block block = null;
+	public ZapblockBlock(VotucraftModElements instance) {
+		super(instance, 14);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(VotucraftItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("zapblock");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
